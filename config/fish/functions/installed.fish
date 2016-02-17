874a15f6127bde643172a015a2f858f09def2c9d
Matthew Sojourner Newton <matt@knosis.org>
2016-02-16 20:48:31 -0800

    whoops, fish uses $argv not $1

--- config/fish/functions/installed.fish
+++ config/fish/functions/installed.fish
@@ -1,6 +1,6 @@
 # Succeed if the given utility is installed. Fail otherwise.
 # For explanations about `which` vs `type` vs `command`, see:
 # http://stackoverflow.com/questions/592620/check-if-a-program-exists-from-a-bash-script/677212#677212
-function installed
-  command -v "$1" >/dev/null 2>&1
+function installed -d 'check if a utility is in the $PATH'
+  command -v $argv >/dev/null 2>&1
 end
