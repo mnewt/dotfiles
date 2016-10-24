a640882acc036a34cef0cb74a5ab80dd094775f0
Matthew Sojourner Newton <matt@knosis.org>
2016-10-24 10:40:23 -0700

    update scripts for clojure, homebrew, ruby. leiningen updates

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,19 +1,20 @@
 {:user
   {:dependencies
-     [[org.clojure/tools.namespace "LATEST"]
-      [pjstadig/humane-test-output "LATEST"]]
+     [[org.clojure/tools.namespace "0.2.11"]
+      [org.clojure/tools.nrepl "0.2.12"]
+      [pjstadig/humane-test-output "0.8.1"]]
    :plugins
-     [[lein-ancient "LATEST"]
-      [lein-pprint "LATEST"]
-      ; [venantius/ultra "LATEST"]
-      [com.jakemccrary/lein-test-refresh "LATEST"]
-      [lein-kibit "LATEST"]
-      [jonase/eastwood "LATEST"]
-      [lein-bikeshed "LATEST"]
-      [lein-cljfmt "LATEST"]
-      [lein-exec "LATEST"]]
+     [[lein-ancient "0.6.10"]
+      [lein-pprint "1.1.2"]
+      [com.jakemccrary/lein-test-refresh "0.17.0"]
+      [lein-kibit "0.1.2"]
+      [jonase/eastwood "0.2.3"]
+      [lein-bikeshed "0.3.0"]
+      [lein-cljfmt "0.5.6"]
+      [lein-exec "0.3.6"]
+      [lein-clean-m2 "0.1.2"]
+      [lein-ns-dep-graph "0.1.0-SNAPSHOT"]]
    :injections
     [(require 'pjstadig.humane-test-output)
      (pjstadig.humane-test-output/activate!)
-     (require '[clojure.tools.namespace.repl :refer [refresh]])]
-   :ultra {:repl false}}}
+     (require '[clojure.tools.namespace.repl :refer [refresh]])]}}
