13e1a22002af1a7f8945f50e3850bcd57d33126d
Matthew Sojourner Newton <matt@mnewton.com>
2019-06-09 22:19:05 -0700

    hammerspoon bugs

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -12,81 +12,90 @@ bind = hs.hotkey.bind
 --------------------------------------------------------------------------------
 
 -- ** Convert a table to a string
-function table.val_to_str ( v )
-   if "string" == type( v ) then
-      v = string.gsub( v, "\n", "\\n" )
-      if string.match( string.gsub(v,"[^'\"]",""), '^"+$' ) then
-         return "'" .. v .. "'"
-      end
-      return '"' .. string.gsub(v,'"', '\\"' ) .. '"'
-   else
-      return "table" == type( v ) and table.tostring( v ) or
-         tostring( v )
-   end
+function table.val_to_str(v)
+    if "string" == type(v) then
+        v = string.gsub(v, "\n", "\\n")
+        if string.match(string.gsub(v, '[^\'"]', ""), '^"+$') then
+            return "'" .. v .. "'"
+        end
+        return '"' .. string.gsub(v, '"', '\\"') .. '"'
+    else
+        return "table" == type(v) and table.tostring(v) or tostring(v)
+    end
 end
 
-function table.key_to_str ( k )
-   if "string" == type( k ) and string.match( k, "^[_%a][_%a%d]*$" ) then
-      return k
-   else
-      return "[" .. table.val_to_str( k ) .. "]"
-   end
+function table.key_to_str(k)
+    if "string" == type(k) and string.match(k, "^[_%a][_%a%d]*$") then
+        return k
+    else
+        return "[" .. table.val_to_str(k) .. "]"
+    end
 end
 
-function table.tostring( tbl )
-   local result, done = {}, {}
-   for k, v in ipairs( tbl ) do
-      table.insert( result, table.val_to_str( v ) )
-      done[ k ] = true
-   end
-   for k, v in pairs( tbl ) do
-      if not done[ k ] then
-         table.insert( result,
-                       table.key_to_str( k ) .. "=" .. table.val_to_str( v ) )
-      end
-   end
-   return "{" .. table.concat( result, "," ) .. "}"
+function table.tostring(tbl)
+    local result, done = {}, {}
+    for k, v in ipairs(tbl) do
+        table.insert(result, table.val_to_str(v))
+        done[k] = true
+    end
+    for k, v in pairs(tbl) do
+        if not done[k] then
+            table.insert(result, table.key_to_str(k) .. "=" .. table.val_to_str(v))
+        end
+    end
+    return "{" .. table.concat(result, ",") .. "}"
 end
 
 -- ** Get output of a bash command
 function os.capture(cmd)
-   local f = assert(io.popen(cmd, 'r'))
-   local s = assert(f:read('*a'))
-   f:close()
-   s = string.gsub(s, '^%s+', '')
-   s = string.gsub(s, '%s+$', '')
-   s = string.gsub(s, '[\n\r]+', ' ')
-   return s
+    local f = assert(io.popen(cmd, "r"))
+    local s = assert(f:read("*a"))
+    f:close()
+    s = string.gsub(s, "^%s+", "")
+    s = string.gsub(s, "%s+$", "")
+    s = string.gsub(s, "[\n\r]+", " ")
+    return s
 end
 
 -- ** Execute and print to console
 function executeAndPrint(cmd)
-   print("Executing command:")
-   print(cmd)
-   out, err = hs.execute(cmd)
-   print(out)
-   if (err ~= nil) then
-      print("Command errored.")
-      print(string.format("Command exited with status: %d", err))
-   end
+    print("Executing command:")
+    print(cmd)
+    out, err = hs.execute(cmd)
+    print(out)
+    if (err ~= nil) then
+        print("Command errored.")
+        print(string.format("Command exited with status: %d", err))
+    end
 end
 
 -- ** Check if file exists
 function file_exists(name)
-   local f=io.open(name,"r")
-   if f~=nil then io.close(f) return true else return false end
+    local f = io.open(name, "r")
+    if f ~= nil then
+        io.close(f)
+        return true
+    else
+        return false
+    end
 end
 
 -- ** Load file if it exists
 function dofile_if(name)
-   if (file_exists(name)) then
-      print("Loading " .. name .. "...")
-      dofile(name)
-   end
+    if (file_exists(name)) then
+        print("Loading " .. name .. "...")
+        dofile(name)
+    end
 end
 
 -- ** Defeat paste blocking
-bind(mash, "v", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)
+bind(
+    mash,
+    "v",
+    function()
+        hs.eventtap.keyStrokes(hs.pasteboard.getContents())
+    end
+)
 
 --------------------------------------------------------------------------------
 -- * Hammerspoon config utlities
@@ -95,20 +104,20 @@ bind(mash, "v", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) e
 -- ** Automatically reload Hammerspoon config
 
 function reloadConfig(files)
-   doReload = false
-   for _,file in pairs(files) do
-      if file:sub(-4) == ".lua" then
-         doReload = true
-      end
-   end
-   if doReload then
-      hs.reload()
-   end
+    doReload = false
+    for _, file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            doReload = true
+        end
+    end
+    if doReload then
+        hs.reload()
+    end
 end
 hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 hs.alert.show("Config loaded")
 
-bind(mash, 'r', hs.reload)
+bind(mash, "r", hs.reload)
 
 -- ** Open Hammerspoon console
 bind(mash, "`", hs.openConsole)
@@ -122,13 +131,13 @@ hs.hotkey.showHotkeys(mash, "/")
 hs.console.smartInsertDeleteEnabled(false)
 -- hs.console.darkMode(true)
 if hs.console.darkMode() then
-   hs.preferencesDarkMode(true)
-   hs.console.outputBackgroundColor({ white = 0 })
-   hs.console.consoleCommandColor({ white = 1 })
+    hs.preferencesDarkMode(true)
+    hs.console.outputBackgroundColor({white = 0})
+    hs.console.consoleCommandColor({white = 1})
 else
-   hs.preferences.darkMode(false)
-   hs.console.windowBackgroundColor({red=.7,blue=.7,green=.7})
-   hs.console.outputBackgroundColor({red=.8,blue=.8,green=.8})
+    hs.preferences.darkMode(false)
+    hs.console.windowBackgroundColor({red = .7, blue = .7, green = .7})
+    hs.console.outputBackgroundColor({red = .8, blue = .8, green = .8})
 end
 
 --------------------------------------------------------------------------------
@@ -136,81 +145,107 @@ end
 --------------------------------------------------------------------------------
 
 -- ** Undo
-local undo = require 'undo'
-bind(mash, "z", function() undo:undo() end)
+local undo = require "undo"
+bind(
+    mash,
+    "z",
+    function()
+        undo:undo()
+    end
+)
 
 -- disable animations
 hs.window.animationDuration = 0
 
-bind(mash, "F", function()
-      local win = hs.window.focusedWindow()
-      local f = win:frame()
-      local screen = win:screen()
-      local max = screen:frame()
+bind(
+    mashshift,
+    "f",
+    function()
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
 
-      f.x = max.x
-      f.y = max.y
-      f.w = max.w
-      f.h = max.h
-      undo:addToStack()
-      win:setFrame(f)
-end)
+        f.x = max.x
+        f.y = max.y
+        f.w = max.w
+        f.h = max.h
+        undo:addToStack()
+        win:setFrame(f)
+    end
+)
 
-bind(mash, "Left", function()
-      local win = hs.window.focusedWindow()
-      local f = win:frame()
-      local screen = win:screen()
-      local max = screen:frame()
+bind(
+    mash,
+    "Left",
+    function()
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
 
-      f.x = max.x
-      f.y = max.y
-      f.w = max.w / 2
-      f.h = max.h
-      undo:addToStack()
-      win:setFrame(f)
-end)
+        f.x = max.x
+        f.y = max.y
+        f.w = max.w / 2
+        f.h = max.h
+        undo:addToStack()
+        win:setFrame(f)
+    end
+)
 
-bind(mash, "Right", function()
-      local win = hs.window.focusedWindow()
-      local f = win:frame()
-      local screen = win:screen()
-      local max = screen:frame()
+bind(
+    mash,
+    "Right",
+    function()
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
 
-      f.x = max.x + (max.w / 2)
-      f.y = max.y
-      f.w = max.w / 2
-      f.h = max.h
-      undo:addToStack()
-      win:setFrame(f)
-end)
+        f.x = max.x + (max.w / 2)
+        f.y = max.y
+        f.w = max.w / 2
+        f.h = max.h
+        undo:addToStack()
+        win:setFrame(f)
+    end
+)
 
-bind(mash, "Up", function()
-      local win = hs.window.focusedWindow()
-      local f = win:frame()
-      local screen = win:screen()
-      local max = screen:frame()
+bind(
+    mash,
+    "Up",
+    function()
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
 
-      f.x = max.x
-      f.y = max.y
-      f.w = max.w
-      f.h = max.h / 2
-      undo:addToStack()
-      win:setFrame(f)
-end)
+        f.x = max.x
+        f.y = max.y
+        f.w = max.w
+        f.h = max.h / 2
+        undo:addToStack()
+        win:setFrame(f)
+    end
+)
 
-bind(mash, "Down", function()
-      local win = hs.window.focusedWindow()
-      local f = win:frame()
-      local screen = win:screen()
-      local max = screen:frame()
+bind(
+    mash,
+    "Down",
+    function()
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
 
-      f.x = max.x
-      f.y = max.y + (max.h / 2)
-      f.w = max.w
-      f.h = max.h / 2
-      undo:addToStack()
-      win:setFrame(f)
-end)
+        f.x = max.x
+        f.y = max.y + (max.h / 2)
+        f.w = max.w
+        f.h = max.h / 2
+        undo:addToStack()
+        win:setFrame(f)
+    end
+)
 
 --------------------------------------------------------------------------------
 -- * Window moving and resizing
@@ -223,48 +258,150 @@ hs.grid.GRIDHEIGHT = 18
 hs.grid.GRIDWIDTH = 18
 
 -- ** Alter gridsize
-bind(mashshift, '=', function() undo:addToStack() hs.grid.adjustHeight(1) end)
-bind(mashshift, '-', function() undo:addToStack() hs.grid.adjustHeight(-1) end)
-bind(mash, '=', function() undo:addToStack() hs.grid.adjustWidth(1) end)
-bind(mash, '-', function() undo:addToStack() hs.grid.adjustWidth(-1) end)
+bind(
+    mashshift,
+    "=",
+    function()
+        undo:addToStack()
+        hs.grid.adjustHeight(1)
+    end
+)
+bind(
+    mashshift,
+    "-",
+    function()
+        undo:addToStack()
+        hs.grid.adjustHeight(-1)
+    end
+)
+bind(
+    mash,
+    "=",
+    function()
+        undo:addToStack()
+        hs.grid.adjustWidth(1)
+    end
+)
+bind(
+    mash,
+    "-",
+    function()
+        undo:addToStack()
+        hs.grid.adjustWidth(-1)
+    end
+)
 
 -- ** Snap windows
-bind(mash, ';', function() undo:addToStack() hs.grid.snap(hs.window.focusedWindow()) end)
-bind(mash, "'", function() undo:addToStack() hs.fnutils.map(hs.window.visibleWindows(), hs.grid.snap) end)
+bind(
+    mash,
+    ";",
+    function()
+        undo:addToStack()
+        hs.grid.snap(hs.window.focusedWindow())
+    end
+)
+bind(
+    mash,
+    "'",
+    function()
+        undo:addToStack()
+        hs.fnutils.map(hs.window.visibleWindows(), hs.grid.snap)
+    end
+)
 
 -- ** Move windows
-bind(mash, 'j', function() undo:addToStack() hs.grid.pushWindowDown() end)
-bind(mash, 'k', function() undo:addToStack() hs.grid.pushWindowUp() end)
-bind(mash, 'h', function() undo:addToStack() hs.grid.pushWindowLeft() end)
-bind(mash, 'l', function() undo:addToStack() hs.grid.pushWindowRight() end)
+bind(
+    mash,
+    "j",
+    function()
+        undo:addToStack()
+        hs.grid.pushWindowDown()
+    end
+)
+bind(
+    mash,
+    "k",
+    function()
+        undo:addToStack()
+        hs.grid.pushWindowUp()
+    end
+)
+bind(
+    mash,
+    "h",
+    function()
+        undo:addToStack()
+        hs.grid.pushWindowLeft()
+    end
+)
+bind(
+    mash,
+    "l",
+    function()
+        undo:addToStack()
+        hs.grid.pushWindowRight()
+    end
+)
 
 -- ** Resize windows
-bind(mashshift, 'k', function() undo:addToStack() hs.grid.resizeWindowShorter() end)
-bind(mashshift, 'j', function() undo:addToStack() hs.grid.resizeWindowTaller() end)
-bind(mashshift, 'l', function() undo:addToStack() hs.grid.resizeWindowWider() end)
-bind(mashshift, 'h', function() undo:addToStack() hs.grid.resizeWindowThinner() end)
+bind(
+    mashshift,
+    "k",
+    function()
+        undo:addToStack()
+        hs.grid.resizeWindowShorter()
+    end
+)
+bind(
+    mashshift,
+    "j",
+    function()
+        undo:addToStack()
+        hs.grid.resizeWindowTaller()
+    end
+)
+bind(
+    mashshift,
+    "l",
+    function()
+        undo:addToStack()
+        hs.grid.resizeWindowWider()
+    end
+)
+bind(
+    mashshift,
+    "h",
+    function()
+        undo:addToStack()
+        hs.grid.resizeWindowThinner()
+    end
+)
 
 -- ** Toggle window zoom (acts like Alt+Shift+GreenPlusButton)
-bind(mash, "m", function()
-                  local win = hs.window.focusedWindow()
-                  local frame = win:frame()
-                  local id = win:id()
+bind(
+    mash,
+    "m",
+    function()
+        local win = hs.window.focusedWindow()
+        local frame = win:frame()
+        local id = win:id()
 
-                  -- init table to save window state
-                  savedwin = savedwin or {}
-                  savedwin[id] = savedwin[id] or {}
+        -- init table to save window state
+        savedwin = savedwin or {}
+        savedwin[id] = savedwin[id] or {}
 
-                  undo:addToStack()
-                  if (savedwin[id].maximized == nil or savedwin[id].maximized == false) then
-                     savedwin[id].frame = frame
-                     savedwin[id].maximized = true
-                     win:maximize()
-                  else
-                     savedwin[id].maximized = false
-                     win:setFrame(savedwin[id].frame)
-                     savedwin[id] = nil
-                  end
-end)
+        undo:addToStack()
+        if (savedwin[id].maximized == nil or savedwin[id].maximized == false) then
+            savedwin[id].frame = frame
+            savedwin[id].maximized = true
+            win:maximize()
+        else
+            savedwin[id].maximized = false
+            win:setFrame(savedwin[id].frame)
+            savedwin[id] = nil
+        end
+    end
+)
 
 -- ** Exposé
 -- This kind of sucks and is buggy
@@ -323,51 +460,65 @@ end)
 -- Stolen from
 -- https://github.com/songchenwen/dotfiles/blob/master/hammerspoon/init.lua
 
-bind(mash, 'q', function() 
-      local c = hs.caffeinate
-      if not c then return end
-      if c.get('displayIdle') or c.get('systemIdle') or c.get('system') then
-         if menuCaff then
-            menuCaffRelease()
-         else
+bind(
+    mash,
+    "q",
+    function()
+        local c = hs.caffeinate
+        if not c then
+            return
+        end
+        if c.get("displayIdle") or c.get("systemIdle") or c.get("system") then
+            if menuCaff then
+                menuCaffRelease()
+            else
+                addMenuCaff()
+                local type
+                if c.get("displayIdle") then
+                    type = "displayIdle"
+                end
+                if c.get("systemIdle") then
+                    type = "systemIdle"
+                end
+                if c.get("system") then
+                    type = "system"
+                end
+                hs.alert("Caffeine already on for " .. type)
+            end
+        else
+            acAndBatt = hs.battery.powerSource() == "Battery Power"
+            c.set("system", true, acAndBatt)
+            hs.alert("Caffeinated " .. (acAndBatt and "" or "on AC Power"))
             addMenuCaff()
-            local type
-            if c.get('displayIdle') then type = 'displayIdle' end
-            if c.get('systemIdle') then type = 'systemIdle' end
-            if c.get('system') then type = 'system' end
-            hs.alert('Caffeine already on for '..type)
-         end
-      else
-         acAndBatt = hs.battery.powerSource() == 'Battery Power'
-         c.set('system', true, acAndBatt)
-         hs.alert('Caffeinated '..(acAndBatt and '' or 'on AC Power'))
-         addMenuCaff()
-      end
-end)
+        end
+    end
+)
 
 function addMenuCaff()
-   menuCaff = hs.menubar.new()
-   menuCaff:setIcon("caffeine-on.pdf") 
-   menuCaff:setClickCallback(menuCaffRelease)
+    menuCaff = hs.menubar.new()
+    menuCaff:setIcon("caffeine-on.pdf")
+    menuCaff:setClickCallback(menuCaffRelease)
 end
 
 function menuCaffRelease()
-   local c = hs.caffeinate
-   if not c then return end
-   if c.get('displayIdle') then
-      c.set('displayIdle', false, true)
-   end
-   if c.get('systemIdle') then
-      c.set('systemIdle', false, true)
-   end
-   if c.get('system') then
-      c.set('system', false, true)
-   end
-   if menuCaff then
-      menuCaff:delete()
-      menuCaff = nil
-   end
-   hs.alert('Decaffeinated')
+    local c = hs.caffeinate
+    if not c then
+        return
+    end
+    if c.get("displayIdle") then
+        c.set("displayIdle", false, true)
+    end
+    if c.get("systemIdle") then
+        c.set("systemIdle", false, true)
+    end
+    if c.get("system") then
+        c.set("system", false, true)
+    end
+    if menuCaff then
+        menuCaff:delete()
+        menuCaff = nil
+    end
+    hs.alert("Decaffeinated")
 end
 
 --------------------------------------------------------------------------------
