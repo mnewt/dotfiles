c531cdbf825de29c334ddd4b9fd380b58bccf8cf
Matthew Sojourner Newton <matt@mnewton.com>
2021-10-04 09:51:39 -0700

    remove fish config

--- .config/fish_config/functions/source_if.fish
+++
@@ -1,5 +0,0 @@
-function source_if -d "If file exists, then source it"
-  if test -e "$argv[1]"
-    source "$argv[1]"
-  end
-end
--- .config/fish_config/functions/vterm_prompt_end.fish
+++
@@ -1,3 +0,0 @@
-function vterm_prompt_end;
-  vterm_printf '51;A'(whoami)'@'(hostname)':'(pwd)
-end
